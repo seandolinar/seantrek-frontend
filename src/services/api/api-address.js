5faeecae4e909af84eec886fb397626149202ca0
seandolinar <sean>
2017-12-06 18:53:02 -0500

    right address

--- src/services/api/api-address.js
+++ src/services/api/api-address.js
@@ -5,33 +5,33 @@ const apiAddress = {
      * Example usage for DataAccessService
      * @returns {*}
      */
-    // getTrips () {
-    //     return DataAccessService.get('//seantrek-api.herokuapp.com/api/trips')
-    // },
-    // getTripOne (id) {
-    //     return DataAccessService.get('//seantrek-api.herokuapp.com/api/tripone/' + id)
-    // },
-    // getState (state) {
-    //     return DataAccessService.get('//seantrek-api.herokuapp.com/api/state/' + state)
-    // },
-    // getPresident (president) {
-    //     return DataAccessService.get('//seantrek-api.herokuapp.com/api/president/' + president)
-    // },
     getTrips () {
-        return DataAccessService.get('//localhost:5000/api/trips')
-    },
-    getTripsFeatured () {
-        return DataAccessService.get('//localhost:5000/api/trips-featured')
+        return DataAccessService.get('//seantrek-api.herokuapp.com/api/trips')
     },
     getTripOne (id) {
-        return DataAccessService.get('//localhost:5000/api/tripone/' + id)
+        return DataAccessService.get('//seantrek-api.herokuapp.com/api/tripone/' + id)
     },
     getState (state) {
-        return DataAccessService.get('//localhost:5000/api/state/' + state)
+        return DataAccessService.get('//seantrek-api.herokuapp.com/api/state/' + state)
     },
     getPresident (president) {
-        return DataAccessService.get('//localhost:5000/api/president/' + president)
-    }
+        return DataAccessService.get('//seantrek-api.herokuapp.com/api/president/' + president)
+    },
+    // getTrips () {
+    //     return DataAccessService.get('//localhost:5000/api/trips')
+    // },
+    // getTripsFeatured () {
+    //     return DataAccessService.get('//localhost:5000/api/trips-featured')
+    // },
+    // getTripOne (id) {
+    //     return DataAccessService.get('//localhost:5000/api/tripone/' + id)
+    // },
+    // getState (state) {
+    //     return DataAccessService.get('//localhost:5000/api/state/' + state)
+    // },
+    // getPresident (president) {
+    //     return DataAccessService.get('//localhost:5000/api/president/' + president)
+    // }
 }
 
 export default apiAddress
