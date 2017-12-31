96574338a806c289591c691f9d920a9fed49d1eb
seandolinar <sean>
2017-12-31 12:21:55 -0500

    new about and refactored styles

--- src/components/4_pages/PageAbout.jsx
+++ src/components/4_pages/PageAbout.jsx
@@ -13,15 +13,37 @@ class PageAbout extends React.Component {
             <div>
                 <h2>About</h2>
                 <div>
+                    <h3>The Setup</h3>
                     <p>
                         In 2011, I flew out to Arizona for work in the middle of February. It was the first time in several years I was out west, and it felt adventourous. 
-                        I also felt the need to drive from Pittsburgh to the Pacfic on my own. I would do that in 2012 after living in Chicago for a summer.
+                        I also felt the need to drive from Pittsburgh to the Pacfic on my own. I have always had an afinity for road trips. As a kid, all our family vacations 
+                        involved piling into the minivan and driving hours to different National Parks and beaches.
+                        
+                        I would do that in 2012 after living in Chicago for a summer.
                     </p>
                     <p>
                         First, I went on three mini road trips in 2011. One to Toronto, Philly, NYC. One to Minneapolis and Fargo. And one down to Orlando.
                     </p>
                     <p>
-                        Finally, in 2012 got around the US.
+                        Finally, in 2012 got around the US. The contract for my video editing job in Chicago ended. Before I started a new job, I took the oppurtunity to drive 
+                        to the Pacific and back.
+                    </p>
+                    <h3>The Hashtag</h3>
+                    <p>
+                        <img src="//stats.seandolinar.com/photos_seantrek/med_500/seantrek_o_05.JPG" className="float-right" />
+                        During the first day of driving, somewhere in Ohio with all the time I had alone in the car, I came up with the hashtag #SeanTrek. It combined one of my 
+                        favorite Sci-Fi franchises with my name. I thought it conveyed the scope and spirit of the trip I was just starting -- one that spanned a continent and was 
+                        full of exploration.
+                    </p>
+                    <p>
+                        On the trip I challenged myself to log the trip on social media creating a detail record using data exhaust from the apps on my iPhone. I also wanted my friends
+                        to be able to follow along as I went around the country. In 2012, Instagram was just starting to take off, so people weren't exhausted from social media lifestyle
+                        posts. It was also four years before the 2016 election, so Twitter still had a lot of non-political content on it. The second part of the challenge would be to 
+                        photograph the trip using on my iPhone 4S. This certainly had its limitations, but the iPhone's camera produced some really gorgeous landscape photos with enough fiddling.
+                    </p>
+                    <h3>This Site</h3>
+                    <p>
+                        It's been five years since the first #SeanTrek, and I've taken many smaller trips since in the same spirit. The idea being to see the US (and Canada)
                     </p>
                 </div>
             </div>
